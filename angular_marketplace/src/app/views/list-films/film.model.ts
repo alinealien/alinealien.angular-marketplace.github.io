e7b8378e5f039c8cde7a1a18c1b1108eeb14f6bd
alinealien <alinebdantunes@gmail.com>
2022-05-27 08:37:16 -0300

    Ajustando Card Film

--- angular_marketplace/src/app/views/list-films/film.model.ts
+++ angular_marketplace/src/app/views/list-films/film.model.ts
@@ -3,18 +3,21 @@ export class Film {
     name: string;
     category: string;
     imgLink: string;
+    sinopse: string;
     price:  number;
 
     constructor(id: number,
         name: string,
         category: string,
             imglink: string,
+            sinopse: string,
             prince: number
             ){
                 this.id = id;
                 this.name = name;
                 this.category = category;
                 this.imgLink = imglink;
+                this.sinopse = sinopse;
                 this.price = prince;
             }
         }
